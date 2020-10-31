5f2c2e5b689cb621ba8c3f6bd59045bb824c70e8
Akshath <akshathmahajan13@gmail.com>
2020-10-31 17:42:26 +0530

    Linked accounts and products

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.conf import settings
+from Products.models import Product
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     amount = models.IntegerField()
-    #products
-
+    products = models.ManyToManyField(Product, through="CartAndProduct")
 class Wishlist(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    #products
+    products = models.ManyToManyField(Product, through="WishlistAndProduct")
+
+class CartAndProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+class WishlistAndProduct(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
