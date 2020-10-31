f3422ff0889e00312f89eca3d5c2f56c417634fa
Akshath <akshathmahajan13@gmail.com>
2020-10-31 20:35:18 +0530

    Made migrations and fixed bugs in models
    Added media root config to settings.py
    Added media url to base urls.py

--- Products/models.py
+++ Products/models.py
@@ -3,30 +3,30 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.conf import settings
 # Create your models here.
 class Collection(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=256)
     description = models.TextField()
     is_featured = models.BooleanField()
     date_created = models.DateTimeField()
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=256)
     num_of_products = models.IntegerField()
 
-class _Set(models.Model):
-    name = models.CharField()
+class ProductSet(models.Model):
+    name = models.CharField(max_length=256)
     is_featured = models.BooleanField()
     special_price = models.IntegerField()
 
 class Product(models.Model):
-    name = models.CharField()
-    image = models.ImageField()
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='Products/ProductImages')
     description = models.TextField()
     discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     avg_rating = models.IntegerField(default=0)
     price = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, default=None, null=True, blank=True)
-    _set = models.ForeignKey(_Set, on_delete=models.SET_NULL, default=None, null=True, blank=True)
+    product_set = models.ForeignKey(ProductSet, on_delete=models.SET_NULL, default=None, null=True, blank=True)
     
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
